34cb2f7dd340b5f6d4252f65e6f2b4ed788e0668
your-name <your-email>
2014-03-21 23:28:34 -0600

    Added if statement for guessing game and params

--- gg.rb
+++ gg.rb
@@ -1,7 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-
+SECRET_NUMBER = rand(100)
 get '/' do
-  erb :index
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+
+
+end
+
+def check_guess(guess)
+  if guess.to_i > SECRET_NUMBER + 5
+    "Way too high!"
+  elsif guess.to_i < SECRET_NUMBER - 5
+    "Way too low!"
+  elsif guess.to_i == SECRET_NUMBER
+    "Correct! You da bomb! The secret number is #{SECRET_NUMBER}"
+  elsif guess.to_i > SECRET_NUMBER
+    "Too high!"
+  elsif guess.to_i < SECRET_NUMBER
+    "Too low!"
+  end
 end
